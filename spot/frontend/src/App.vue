8fbeb2fb269a3a92b9cb48edbc8a1e3b11a8b8d9
sxzuzv <pofoxz@gmail.com>
2024-05-30 15:23:54 +0900

    Design: 사이드 바 UI 구성 및 우측 resizable 적용 (vue-resizable 라이브러리 활용)

--- spot/frontend/src/App.vue
+++ spot/frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <MainMap />
+    <SideBar class="side-bar" />
   </div>
 </template>
 
 <script>
 import MainMap from "./components/MainMap.vue";
+import SideBar from "./components/SideBar.vue";
 
 export default {
   name: 'App',
   components: {
-    MainMap
+    MainMap,
+    SideBar
   }
 }
 </script>
@@ -23,5 +26,12 @@ export default {
   right: 0;
   bottom: 0;
   left: 0;
+
+  > .side-bar {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+  }
 }
 </style>
---
+++ spot/frontend/src/components/SideBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="side-bar-wrapper">
+    <!-- active="['r']"의 의미는? 우측 resize(크기 조정)를 활성화한다. -->
+    <VueResizable
+      class="resizable-side-bar"
+      :width="500"
+      :min-width="500"
+      :max-width="Infinity"
+      :active="['r']"
+    >
+    <!-- 컴포넌트 해온 vue-resizable 하위에 <div> 태그를 넣는다. -->
+    <div class="side-bar">
+    </div>
+    </VueResizable>
+  </div>
+</template>
+
+<script>
+import VueResizable from 'vue-resizable';
+
+export default {
+  name: 'SideBar',
+  components: {
+    VueResizable
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.side-bar-wrapper {
+  > .resizable-side-bar {
+    > .side-bar {
+      background-color: #000000;
+      opacity: 0.5; // 투명도
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+
+</style>
