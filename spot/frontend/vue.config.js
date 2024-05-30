71bf216ac5ba088c9b8f03239cd8a669251bd6cb
sxzuzv <pofoxz@gmail.com>
2024-05-30 11:46:50 +0900

    Feat: SpringBoot 및 Vue Proxy 연결 완료 (임의 API 생성 및 테스트)

--- spot/frontend/vue.config.js
+++ spot/frontend/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: "../src/main/resources/static",  // Vue.js Build 결과물을 만들 타겟 디렉토리
+  devServer: {
+    proxy: {
+      '/api': {
+        // '/api' 경로로 접근하면 8080 port(스프링 서버)로 요청을 넘긴다.
+        target: 'http://localhost:8080',
+        changeOrigin: true // cross origin 허용
+      }
+    }
+  }
 })
