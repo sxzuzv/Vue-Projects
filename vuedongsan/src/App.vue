6a164664de31d2a4438d1682d9f6049d5ebf159e
sxzuzv <pofoxz@gmail.com>
2024-06-14 09:00:42 +0900

    commit

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,47 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <img alt="Vue logo" src="./assets/logo.svg">
+    <div>
+      <h4>{{ name }}원룸</h4>
+      <!-- {{ 데이터명 }}: 데이터 바인딩 -->
+      <p>{{ price }}만원</p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <h4>{{ name }}원룸</h4>
+      <p>{{ price }}만원</p>
+    </div>
+    <div>
+      <h4>{{ name }}원룸</h4>
+      <p>{{ price }}만원</p>
+    </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+    // 변수 등의 데이터를 보관한다.
+      name: '행복',
+      price: 70
+    }
+  },
+  components: {
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
 }
+</script>
+
+<style>
+
 </style>
