aab7cd1a8758396c3deae3a93343d9ba77fe2728
sxzuzv <pofoxz@gmail.com>
2024-06-14 15:32:36 +0900

    Add: 모달창 컴포넌트화 및 Props 적용

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,51 +1,29 @@
 <template>
-  <!-- 모달창 -->
-  <div class="black-bg" v-if="modalState == true">
-    <div class="white-bg">
-      <img :src="onerooms[roomNum].image" class="room-img">
-      <h2>[{{ onerooms[roomNum].title }}] {{ onerooms[roomNum].price }}만원</h2>
-      <p>{{ onerooms[roomNum].content }}</p> <br>
-      <button @click="modalState = false">닫기</button>
-    </div>
-  </div>
+  <!-- 모달창: Props -->
+  <Modal :onerooms="onerooms" :roomNum="roomNum" :modalState="modalState" />
 
   <!-- 상단 메뉴 -->
   <div class="menu">
-    <!-- v-for: 반복문 -->
     <a v-for="m in menu" :key="m"> {{ m }} </a>
-    <!-- (a, b): b는 0부터 1씩 증가하는 정수 -->
-<!--    <a v-for="(m, i) in menu" :key="i"> {{ m }} </a>-->
   </div>
   <br>
 
   <!-- 홍보 배너: Component -->
   <Discount />
 
+  <!-- 매물 정보 -->
   <div v-for="(r, i) in onerooms" :key="i">
     <img :src="r.image" class="room-img">
     <h2 @click="modalState = true; roomNum = i">[{{ r.title }}] {{ r.price }}만원</h2>
     <p>{{ r.content }}</p>
     <br>
   </div>
-
-<!--  <div v-for="(p, i) in products" :key="i">-->
-<!--    <img src="./assets/img.png" class="room-img">-->
-<!--    <h2>[{{ p }}] {{ price[i] }}</h2>-->
-<!--    &lt;!&ndash; 이벤트 핸들러: click을 감지하여 특정 행동을 실행 &ndash;&gt;-->
-<!--    <button @click="addCount(i)">허위 매물 신고</button> <span>신고 수: {{ count[i] }}</span>-->
-<!--  </div>-->
-
-<!--  <div>-->
-<!--    <img src="./assets/img.png" class="room-img">-->
-<!--    <h2 @click="modalState = true">[{{ products[0] }}] {{ price[0] }}</h2>-->
-<!--    &lt;!&ndash; 이벤트 핸들러: click을 감지하여 특정 행동을 실행 &ndash;&gt;-->
-<!--    <button @click="addCount(0)">허위 매물 신고</button> <span>신고 수: {{ count[0] }}</span>-->
-<!--  </div>-->
 </template>
 
 <script>
   import roomInfo from './assets/oneroom.js';
   import Discount from "./components/Discount.vue";
+  import Modal from "./components/Modal.vue";
 
   export default {
     name: 'App',
@@ -67,7 +45,8 @@
       }
     },
     components: {
-      Discount: Discount
+      Discount: Discount,
+      Modal: Modal
     }
   }
 </script>
@@ -97,23 +76,5 @@
 .room-img {
   width: 100%;
   margin-top: 10px;
-}
-
-/* 모달창 스타일 적용 */
-body {
-  margin: 0;
-}
-div {
-  box-sizing: border-box;
-}
-.black-bg {
-  width: 100%; height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  position: fixed; padding: 20px;
-}
-.white-bg {
-  width: 100%; background: white;
-  border-radius: 8px;
-  padding: 20px;
 }
 </style>
---
+++ vuedongsan/src/components/Modal.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- 모달창 -->
+  <div class="black-bg" v-if="modalState == true">
+    <div class="white-bg">
+      <img :src="onerooms[roomNum].image" class="room-img">
+      <h2>[{{ onerooms[roomNum].title }}] {{ onerooms[roomNum].price }}만원</h2>
+      <p>{{ onerooms[roomNum].content }}</p> <br>
+<!--      <button @click="modalState = false">닫기</button>-->
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Modal',
+    props: {
+      onerooms: Array,
+      roomNum: Number,
+      modalState: Boolean // Props를 통해 가져온 데이터는 수정 불가(read only)
+    }
+  }
+</script>
+
+<style>
+  /* 모달창 스타일 적용 */
+  body {
+    margin: 0;
+  }
+  div {
+    box-sizing: border-box;
+  }
+  .black-bg {
+    width: 100%; height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed; padding: 20px;
+  }
+  .white-bg {
+    width: 100%; background: white;
+    border-radius: 8px;
+    padding: 20px;
+  }
+</style>
