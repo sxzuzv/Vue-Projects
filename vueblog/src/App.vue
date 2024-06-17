c0e3ca189fb656d5eccedd17a74be8fc87ba1568
sxzuzv <pofoxz@gmail.com>
2024-06-17 13:00:37 +0900

    Add: 블로그 콘텐츠 적용 및 출력

--- vueblog/src/App.vue
+++ vueblog/src/App.vue
@@ -1,12 +1,87 @@
 <template>
+  <div class="container mt-4">
+    <!-- navbar start -->
+    <nav class="navbar bg-body-tertiary fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">vueblog</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">menu</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex mt-3" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- navbar end -->
+  </div>
 
+  <List :blogs="blogs" />
+
+  <div class="container mt-4">
+    <!-- card start -->
+    <div class="card" style="width: 50%; display: inline-block">
+      <img src="./assets/nongdam.jpeg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    <div class="card" style="width: 50%; display: inline-block">
+      <img src="./assets/nongdam.jpeg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    <!-- card end -->
+  </div>
 </template>
 
 <script>
+import List from './components/List.vue';
+import blog from './assets/blog.js';
+
 export default {
   name: 'App',
+  data() {
+    return {
+      blogs: blog
+    }
+  },
   components: {
-
+    List: List
   }
 }
 </script>
@@ -16,6 +91,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
   text-align: center;
+  margin-top: 0;
+  width: 100%;
 }
 </style>
---
+++ vueblog/src/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-for="(a, i) in blogs" :key="i">
+    <h2>{{ a.title }}</h2>
+    <p>{{ a.content }}</p>
+    <p>{{ a.date }}</p>
+    <br>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'List',
+   props: {
+     blogs: Array
+   }
+}
+</script>
+
+<style>
+
+</style>
